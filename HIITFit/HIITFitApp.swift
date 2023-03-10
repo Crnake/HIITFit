24b43f0f344029f3460b10d85521d682bd155970
Crnake <liu-huo@live.com@qq.com>
2023-03-10 18:26:15 +0800

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by rnake on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
