21329049eb13aeafe5d27ea4c09fd7f348289504
Crnake <liu-huo@live.com>
2023-03-11 11:25:54 +0800

    chapter 2

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-@main
+@main // 表明了遵从App协议的自定义struct提供了程序的进入点，App协议提供了启动应用程序的main方法的默认实现
 struct HIITFitApp: App {
+    // App必须实现computed property:body并且返回一个Scene， Scene是视图层次的根视图的容器
     var body: some Scene {
+        // 在iOS中一种常见的做法是根据用户是否登陆来决定根视图
+        // 在iOS中视图层次填充整个屏幕，macOS和iPadOS中WindowGroup可以管理多个视图
         WindowGroup {
             ContentView()
         }
---
+++ HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,23 @@
+//
+//  ExerciseView.swift
+//  HIITFit
+//
+//  Created by rnake on 2023/3/11.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let index: Int
+    var body: some View {
+        Text(exerciseNames[index])
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(index: 0)
+    }
+}
